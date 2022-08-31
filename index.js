1aaf9d840efc300f2ec6324b503573dca66d3104
Byron <byronogis@outlook.com>
2022-08-31 22:27:57 +0800

    feat: add clear btn

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const iframe = document.querySelector('.dc-iframe')
 
-const dateEl = document.querySelector('#date')
+const dateEl = document.querySelector('#dhd-date')
+console.dir(dateEl)
 dateEl.addEventListener('change', e => {
   let date = e.target.value + ''
   date &&= date.replace(/-/g, '')
+  date ||= 'basic/desc'
   console.log(date);
 
   iframe.setAttribute('src', `./daily/${date}.html`)
+})
+
+const dateClearEl = document.querySelector('.dhd-clear')
+dateClearEl.addEventListener('click', e => {
+  dateEl.value = ''
+  iframe.setAttribute('src', `./daily/basic/desc.html`)
 })
